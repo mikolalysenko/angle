081f42960c2418727a1904e09fb38375e1acd05f
Mikola Lysenko <mikolalysenko@gmail.com>
2015-07-28 10:23:43 -0500

    turn off inlining, was causing build to fail during link on Linux

--- src/third_party/murmurhash/MurmurHash3.cpp
+++ src/third_party/murmurhash/MurmurHash3.cpp
@@ -16,7 +16,7 @@
 
 #if defined(_MSC_VER)
 
-#define FORCE_INLINE	__forceinline
+#define FORCE_INLINE	
 
 #include <stdlib.h>
 
@@ -31,7 +31,7 @@
 
 // Ignore GCC force inline warnings
 #pragma GCC diagnostic ignored "-Wattributes"
-#define	FORCE_INLINE __attribute__((always_inline))
+#define	FORCE_INLINE
 
 inline uint32_t rotl32 ( uint32_t x, int8_t r )
 {
@@ -116,9 +116,9 @@ void MurmurHash3_x86_32 ( const void * key, int len,
     k1 *= c1;
     k1 = ROTL32(k1,15);
     k1 *= c2;
-    
+
     h1 ^= k1;
-    h1 = ROTL32(h1,13); 
+    h1 = ROTL32(h1,13);
     h1 = h1*5+0xe6546b64;
   }
 
@@ -145,7 +145,7 @@ void MurmurHash3_x86_32 ( const void * key, int len,
   h1 = fmix(h1);
 
   *(uint32_t*)out = h1;
-} 
+}
 
 //-----------------------------------------------------------------------------
 
@@ -160,9 +160,9 @@ void MurmurHash3_x86_128 ( const void * key, const int len,
   uint32_t h3 = seed;
   uint32_t h4 = seed;
 
-  const uint32_t c1 = 0x239b961b; 
+  const uint32_t c1 = 0x239b961b;
   const uint32_t c2 = 0xab0e9789;
-  const uint32_t c3 = 0x38b34ae5; 
+  const uint32_t c3 = 0x38b34ae5;
   const uint32_t c4 = 0xa1e38b93;
 
   //----------
